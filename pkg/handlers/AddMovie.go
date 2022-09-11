d54cbb86d792642fd2a3522f4d2a5d3204656cf5
Vadim S <di3mpi3@gmail.com>
2022-09-11 17:03:56 +0200

    Change json return

--- pkg/handlers/AddMovie.go
+++ pkg/handlers/AddMovie.go
@@ -22,14 +22,12 @@ func (h handler) AddMovie(w http.ResponseWriter, r *http.Request) {
 	var movie models.Movie
 	json.Unmarshal(body, &movie)
 
-	// Append to the movie table
 	if result := h.DB.Create(&movie); result.Error != nil {
 		fmt.Println(result.Error)
 	}
 
-	// Send a 201 created response
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	// {"id":"tt0368226","name":"The Room","year":"2003","score":"3.7"}
+
 	json.NewEncoder(w).Encode(movie)
 }
--- pkg/models/movie.go
+++ pkg/models/movie.go
@@ -1,8 +1,8 @@
 package models
 
 type Movie struct {
-	IMDb_id string  `gorm:"column:IMDb_id;primaryKey" json:"imdb_id"`
-	Title   string  `json:"title"`
-	Rating  float64 `json:"rating"`
-	Year    int     `json:"year"`
+	IMDb_id      string  `gorm:"column:IMDb_id;primaryKey" json:"id"`
+	Title        string  `json:"name"`
+	Year         int     `json:"year"`
+	Rating       float64 `json:"score"`
 }
