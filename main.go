9aff36a3778802ecd2a861174da36c8f8e374cbe
Vadim S <di3mpi3@gmail.com>
2022-09-12 13:36:53 +0200

    rename and formatting

--- main.go
+++ main.go
@@ -11,11 +11,9 @@ import (
 
 func main() {
 	DB := db.Init()
-	cli.CliHandler()
-
 	h := handlers.New(DB)
-	router := mux.NewRouter()
 
+	router := mux.NewRouter()
 	router.HandleFunc("/movies", h.GetAllMovies).Methods(http.MethodGet)
 	router.HandleFunc("/movies/{id}", h.GetMovie).Methods(http.MethodGet)
 	router.HandleFunc("/titles", h.GetAllMovieTitles).Methods(http.MethodGet)
@@ -24,5 +22,9 @@ func main() {
 	router.HandleFunc("/movies/{id}", h.UpdateMovie).Methods(http.MethodPut)
 	router.HandleFunc("/movies/{id}", h.DeleteMovie).Methods(http.MethodDelete)
 
-	http.ListenAndServe(":8090", router)
+	go func() {
+		http.ListenAndServe(":8090", router)
+	}()
+
+	cli.CliHandler()
 }
