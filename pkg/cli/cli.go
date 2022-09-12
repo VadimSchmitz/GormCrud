20c8faba6231897f9039984394792eb5d51f8e48
Vadim S <di3mpi3@gmail.com>
2022-09-13 00:40:56 +0200

    Implement a worker pool

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -18,25 +18,52 @@ func CliHandler(done chan bool) {
 	}
 
 	if arguments[0] == "summaries" {
-		
-		var IMDb_id []*models.IMDb_id		
+		var IMDb_id []*models.IMDb_id
 		idUrl := "http://localhost:8090/ids"
 		h.ProcessRequest(idUrl, &IMDb_id)
 
-		var Plot_summary *models.Plot		
+		numJobs := len(IMDb_id)
 
-		for _, id := range IMDb_id {
-			omdbUrl := "http://www.omdbapi.com/?i=" + id.IMDb_id + "&apikey=183b9a26"
-			h.ProcessRequest(omdbUrl, &Plot_summary)
+		jobs := make(chan *models.IMDb_id, numJobs)
+		results := make(chan string, numJobs)
 
-			var jsonStr = []byte(`{"plot": "` + Plot_summary.Plot_summary + `"}`)
-			req, _ := http.NewRequest("PATCH", "http://localhost:8090/movies/" + id.IMDb_id, bytes.NewBuffer(jsonStr))
-			
-			client := &http.Client{}
-			client.Do(req)
+		for w := 1; w <= 5; w++ {
+			go worker(w, jobs, results)
 		}
 
-		fmt.Println("Summaries added")
+		for _, imdb_id := range IMDb_id {
+			jobs <- imdb_id
+		}
+
+		close(jobs)
+
+		for a := 1; a <= numJobs; a++ {
+			fmt.Printf("Wrote summary for %s to database\n", <-results)
+		}
+
 		done <- true
+	}
+}
+
+func worker(wId int, jobs <-chan *models.IMDb_id, results chan<- string) {
+	for imdb_id := range jobs {
+		fmt.Printf("Worker %d getting summary for %s \n", wId, imdb_id.IMDb_id)
+
+		fmt.Println(imdb_id.IMDb_id)
+		omdbUrl := "http://www.omdbapi.com/?i=" + imdb_id.IMDb_id + "&apikey=183b9a26"
+		var plot *models.Plot
+
+		fmt.Printf("Worker %d received summary for %s \n", wId, imdb_id.IMDb_id)
+
+		h.ProcessRequest(omdbUrl, &plot)
+
+		var jsonStr = []byte(`{"plot": "` + plot.Plot_summary + `"}`)
+		req, _ := http.NewRequest("PATCH", "http://localhost:8090/movies/"+imdb_id.IMDb_id, bytes.NewBuffer(jsonStr))
+
+		client := &http.Client{}
+		client.Do(req)
+
+		
+		results <- imdb_id.IMDb_id
 	}
 }
