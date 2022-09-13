83edd801928a6e6274954f08c99673c615003184
Vadim S <di3mpi3@gmail.com>
2022-09-13 20:10:36 +0200

    bozo hours

--- pkg/handlers/routes/PatchMovie.go
+++ pkg/handlers/routes/PatchMovie.go
@@ -43,8 +43,8 @@ func (h handler) UpdateMovie(w http.ResponseWriter, r *http.Request) {
 	if updatedMovie.Rating != 0 {
 		movie.Rating = updatedMovie.Rating
 	}
-	if updatedMovie.Summary != "" {
-		movie.Summary = updatedMovie.Summary
+	if updatedMovie.Plot_summary != "" {
+		movie.Plot_summary = updatedMovie.Plot_summary
 	}
 
 	h.DB.Model(&movie).Updates(models.Movie{
@@ -52,7 +52,7 @@ func (h handler) UpdateMovie(w http.ResponseWriter, r *http.Request) {
 		Title: movie.Title, 
 		Year: movie.Year, 
 		Rating: movie.Rating, 
-		Summary: movie.Summary})
+		Plot_summary: movie.Plot_summary})
 
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
