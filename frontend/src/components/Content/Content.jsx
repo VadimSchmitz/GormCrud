0abfafe132ce6996ba7a5cbff6b0a9497ff5a363
Vadim S <di3mpi3@gmail.com>
2022-09-15 20:17:59 +0200

    Keep y location on change

--- frontend/src/components/Content/Content.jsx
+++ frontend/src/components/Content/Content.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
-import Pagination from "./Pagination";
+import { useState, useEffect } from "react";
+import Pagination from "./MovieEntry/Pagination";
 import PaginatedMovies from "./MovieEntry/PaginatedMovies";
 
 export default function Content({ movies }) {
-
   const [currentPage, setCurrentPage] = useState(1);
   const [recordsPerPage] = useState(5);
 
@@ -11,6 +10,14 @@ export default function Content({ movies }) {
   const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
   const currentRecords = movies.slice(indexOfFirstRecord, indexOfLastRecord);
   const nPages = Math.ceil(movies.length / recordsPerPage);
+
+  useEffect(() => {
+    const scrollPosition = sessionStorage.getItem("scrollPosition");
+    if (scrollPosition) {
+      window.scrollTo(0, parseInt(scrollPosition, 10));
+      sessionStorage.removeItem("scrollPosition");
+    }
+  }, [currentPage]);
 
   return (
     <div>
